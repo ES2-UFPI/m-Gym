e5df2dedc7a5c5f901943d04fbbb393c3dece8d4
PedroFeitosaSoares <pedro.soares@ufpi.edu.br>
2025-05-14 21:12:54 -0300

    close #10 garantindo a unicidade do email no cadastro do usuário

---
+++ backend/src/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from src.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    login = Column(String(25), nullable=False, unique= False)
+    email = Column(String(255), nullable=False, unique=True)
+    # email = Column(String(255), primary_key=True, index=True, unique=True)
+    password = Column(String(255), nullable=False)
+    photo = Column(String(255), nullable=True)
--- backend/src/server.py
+++ backend/src/server.py
@@ -1,27 +1,36 @@
-from fastapi import FastAPI, HTTPException
-from src.schemas.user import UserCreate 
-from typing import List
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+from src.schemas.user import UserCreate
+from src.models.user import User
+from src.database import get_db
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Configuração do CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # ou ["*"] durante desenvolvimento
+    allow_origins=["http://localhost:3000", "http://localhost:8000"],  # ou ["*"] durante desenvolvimento
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app_db = []
-
 @app.post("/usuarios", status_code=201)
-def criar_usuario(user: UserCreate):
-    # Verifica se e-mail já está em uso
-    for existing_user in app_db:
-        if existing_user['email'] == user.email:
-            raise HTTPException(status_code=400, detail="E-mail já cadastrado.")
+def criar_usuario(user: UserCreate, db: Session = Depends(get_db)):
+    # Verifica se o e-mail já está em uso
+    if db.query(User).filter(User.email == user.email).first():
+        raise HTTPException(status_code=400, detail="E-mail já cadastrado.")
     
-    novo_usuario = user.dict()
-    app_db.append(novo_usuario)
+    
+    # Cria um novo usuário
+    novo_usuario = User(
+        login=user.login,
+        email=user.email,
+        password=user.password,  # Certifique-se de usar hash para senhas
+        photo=user.photo
+    )
+    db.add(novo_usuario)
+    db.commit()
+    db.refresh(novo_usuario)
     return {"message": "Usuário criado com sucesso!", "usuario": novo_usuario}
