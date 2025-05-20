af6359738a1e9c5c7155797707cb51b719510aef
Felps_LDL <felipe.lages.lima@gmail.com>
2025-05-20 09:18:45 -0300

    Model de perfil e relacionamento com usuário

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from src.database import Base
 
 class User(Base):
@@ -10,3 +11,4 @@ class User(Base):
     # email = Column(String(255), primary_key=True, index=True, unique=True)
     password = Column(String(255), nullable=False)
     photo = Column(String(255), nullable=True)
+    profile = relationship("Profile", uselist=False, back_populates="user")
